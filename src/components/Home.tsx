8b1ff76a1991d86c135c490e6827ba60c4c0953d
ayebravo <ayelenbravo@gmail.com>
2022-02-24 18:16:49 -0600

    Created Home component and a couple of directories
    I also added the logo

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import Logo from '../assets/Logo.png';
+import '../styles/Home.css';
+
+const Home = () => {
+    return (
+        <img src={Logo} className="Home-logo" alt="logo" />
+    );
+};
+
+export default Home;
