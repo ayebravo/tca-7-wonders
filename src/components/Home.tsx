e92b115a018401b9bc5a36a3801940ab6a885567
ayebravo <ayelenbravo@gmail.com>
2022-02-24 18:54:01 -0600

    Installed Material Design icons and added some buttons to the Home page
    Command to install icons: npm install @mui/icons-material

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,27 @@
 import Logo from '../assets/Logo.png';
+import Button from '@mui/material/Button';
+import CasinoOutlinedIcon from '@mui/icons-material/CasinoOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
+import Stack from '@mui/material/Stack';
 import '../styles/Home.css';
 
 const Home = () => {
     return (
-        <img src={Logo} className="Home-logo" alt="logo" />
+        <>
+            <img src={Logo} className="Home-logo" alt="logo" />
+            <Stack className="Home-buttons-section" spacing={3}>
+                <Button variant="contained" startIcon={<CasinoOutlinedIcon />} size="large" color="success">
+                    New Game
+                </Button>
+                <Button variant="contained" startIcon={<AnalyticsOutlinedIcon />} size="large" color="success">
+                    Fun Facts
+                </Button>
+                <Button variant="contained" startIcon={<TimelineOutlinedIcon />} size="large" color="success">
+                    Games History
+                </Button>
+            </Stack>
+        </>
     );
 };
 
